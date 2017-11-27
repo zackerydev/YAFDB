840b6bf4c163ce856a59b4f377d8c29ac50a714b
Zackery Griesinger <zgriesinger@gmail.com>
2017-11-27 13:31:49 -0600

    Season type for player and more readme updates

--- README.md
+++ README.md
@@ -7,12 +7,15 @@ The only program *needed* for this project is Node.js which you can find here: h
   2. Clone the repository into a directory of your choice
   3. Navigate to the directory where you just cloned the repo, with Cmder this will be `cd C:\your\repo\dir`
   4. Run the command `npm install` and node will install all the packages needed
+  4.5 If not installed run `npm install -g nodemon`
   5. Navigate to the `/server` directory and run `npm install` again to get the packages the server needs
   5. Now that the packages are all installed run the command `npm run dev-api` this starts a local development Express.js "Server" on your machine that mimics the way the actual server handles API requests so we can run the website locally. It will also automatically reload when server files change
   6. Open a new tab in Cmder (or a new command prompt window) and run `npm start` in the same directory. This starts up the actual react app and loads the front end. It should automatically open a window for you and reload on changes.
   7. To stop, all you need to do is press CTRL+C and type `y` to the prompt. 
 
-NOTE: You will need to set up a config file called config.json
+NOTE: You will need to set up a config file called config.json using the config template
+Then run the .sql file in the directory to update the database with data to run locally.
+
 After the first ime run steps 5 - 7 to startup the app to view your changes.
 
 ## Project Structure
--- server/routes/db.js
+++ server/routes/db.js
@@ -156,7 +156,7 @@ router.get('/teams', function(req, res, next) {
 	//res.send('respond with a resource')
 	//We will use res.json(DATA_FROM_DB) to send data back
 	var connection = mysql.createConnection(cfg);
-	var SQL_QUERY = 'SELECT id, name, city, state, code, division, conference from team;'
+	var SQL_QUERY = 'SELECT id, name, city, state, code, division, conference FROM team;'
 	connection.connect();
 	connection.query(SQL_QUERY, (error, results, fields) => {
 		if(error) console.log(error)
@@ -214,7 +214,7 @@ router.get('/player/stats', function(req, res, next) {
 	//We will use res.json(DATA_FROM_DB) to send data back
 	var connection = mysql.createConnection(cfg);
 	console.log(req.query)
-	var SQL = ` SELECT first_name, last_name, team_id, season_year, sum(passing_yards), sum(passing_attempts), sum(passing_completions)
+	var SQL = ` SELECT first_name, last_name, team_id, season_type, season_year, sum(passing_yards), sum(passing_attempts), sum(passing_completions)
 				, sum(rushing_yards), sum(rushing_attempts), sum(rushing_touchdowns), sum(receiving_yards)
 				, sum(receiving_catches), sum(receiving_targets), sum(receiving_touchdowns), sum(tackles)
 				, sum(interceptions_thrown), sum(interceptions_caught), sum(fumbles), sum(fumbles_lost)
@@ -222,12 +222,12 @@ router.get('/player/stats', function(req, res, next) {
 				, sum(kicking_fg_tried), sum(kicking_pat_made), sum(kicking_pat_tried), sum(punts)
 				, sum(punting_yds), sum(kick_return_yds), sum(kick_return_tds), sum(punt_return_yds)
 				from player_game_stats 
-				INNER JOIN (select distinct id, season_year from game) as ID_SEASON 
+				INNER JOIN (select distinct id, season_type, season_year from game) as ID_SEASON 
 				ON player_game_stats.game_id = ID_SEASON.id
 				INNER JOIN (select first_name, last_name, id from player 
 				WHERE first_name = ? AND last_name = ?) as SELECTED_PLAYER
 				ON SELECTED_PLAYER.id = player_id
-				GROUP BY season_year, last_name, team_id;`
+				GROUP BY season_year, FIELD(season_type, 'PRE', 'REG', 'POST'), last_name, team_id, season_type;`
 	var inserts = [req.query.first_name, req.query.last_name];
 	SQL = mysql.format(SQL, inserts);
 	//console.log(SQL)
